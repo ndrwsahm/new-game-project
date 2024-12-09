5b6f029018b2f1cab6e83d4b28cc001da4350509
Andrew <andrew_sahm@yahoo.com>
2024-12-08 19:01:57 -0500

    Added initial battle movement

--- scripts/scene_trigger.gd
+++ scripts/scene_trigger.gd
@@ -4,4 +4,6 @@ class_name SceneTrigger extends Area2D
 
 func _on_body_entered(body) -> void:
 	if body is Player:
+		SceneTransition.load_scene()
 		scene_manager.change_scene(get_owner(), connected_scene)
+		SceneTransition.fade_scene()
